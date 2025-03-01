d96b7fbbbd4bc2a80302941a217f98f1f20cd4d9
Erivelter <eriveltervilasboas@hotmail.com>
2025-03-01 18:00:29 -0300

    Refactor canvas grid and class for grid management

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
+import { Board } from "./src/assets/js/board_generator.js";
+
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+
 const largura = 496;
 const altura = 496;
-console.log(window.devicePixelRatio);
-canvas.width = largura * window.devicePixelRatio;
-canvas.height = altura * window.devicePixelRatio;
-canvas.style.width = largura + "px";
-canvas.style.height = altura + "px";
-ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
-// make a grid with canvas
-for (let i=0; i<8; i++) {
-    for (let j=0; j<8; j++){
-        ctx.fillStyle = ((i+j) % 2 === 0)? "#EBECD0" : "#779556";
-        ctx.fillRect(i*largura/8, j*altura/8, canvas.width/8, canvas.height/8);
-    }
-}
+const board = new Board(canvas, largura, altura,8);
+board.draw();
+
 
 
---
+++ src/assets/js/board_generator.js
@@ -0,0 +1,32 @@
+
+export class Board {
+    constructor(canvas, width, height,blocks) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.width = width;
+        this.height = height;
+        this.blocks = blocks;
+
+        this.canvas.width = width * window.devicePixelRatio;
+        this.canvas.height = height * window.devicePixelRatio;
+        this.canvas.style.width = width + "px";
+        this.canvas.style.height = height + "px";
+
+        this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    }
+    draw() {
+    // make a grid with canvas
+    for (let i=0; i<this.blocks; i++) {
+        for (let j=0; j<this.blocks; j++){
+            this.ctx.fillStyle = ((i+j) % 2 === 0)? "#EBECD0" : "#779556";
+            this.ctx.fillRect(
+                i*this.width/this.blocks,
+                j*this.height/this.blocks,
+                this.width/this.blocks,
+                this.height/this.blocks);
+    }
+}
+    }
+}
+
+
